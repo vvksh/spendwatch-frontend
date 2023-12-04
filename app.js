14386587cee69209ac6da65fcab30a4084b63af4
viveks <vivek.sah32@gmail.com>
2023-12-03 23:42:03 -0800

    Add password prompt

--- app.js
+++ app.js
@@ -9,89 +9,132 @@ async function fetchData(url) {
         return data;
     } catch (error) {
         console.error('Error fetching data:', error);
-        return null; // Return null in case of an error
+        throw new Error(error.message);
     }
 }
 
 // API endpoints
-const monthlyExpensesEndpoint = 'https://example.com/api/monthly-expenses';
-const categoryExpensesEndpoint = 'https://example.com/api/category-expenses';
-const creditCardExpensesEndpoint = 'https://example.com/api/credit-card-expenses';
+var monthlyExpensesEndpoint = 'https://spendwatch-api.viveks.xyz/expenses?groupBy=month';
+var categoryExpensesEndpoint = 'https://spendwatch-api.viveks.xyz/expenses?groupBy=category';
+var creditCardExpensesEndpoint = 'https://spendwatch-api.viveks.xyz/expenses?groupBy=credit_card';
 
-// Fetch data from the backend API
-Promise.all([
-    fetchData(monthlyExpensesEndpoint),
-    fetchData(categoryExpensesEndpoint),
-    fetchData(creditCardExpensesEndpoint)
-]).then(([monthlyExpenses, categoryExpenses, creditCardExpenses]) => {
-    // Bar Chart
-    const barCtx = document.getElementById('barChart').getContext('2d');
-    const barChart = new Chart(barCtx, {
-        type: 'bar',
-        data: {
-            labels: monthlyExpenses ? Object.keys(monthlyExpenses) : [],
-            datasets: [{
-                label: 'Monthly Expenses',
-                data: monthlyExpenses ? Object.values(monthlyExpenses) : [],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+const monthOrder = {
+    January: 0,
+    February: 1,
+    March: 2,
+    April: 3,
+    May: 4,
+    June: 5,
+    July: 6,
+    August: 7,
+    September: 8,
+    October: 9,
+    November: 10,
+    December: 11
+};
+
+function submitPassword() {
+    // Get the entered password
+    var password = document.getElementById("password").value;
+    monthlyExpensesEndpoint = monthlyExpensesEndpoint + "&pwd=" + password
+    categoryExpensesEndpoint = categoryExpensesEndpoint + "&pwd=" + password
+    creditCardExpensesEndpoint = creditCardExpensesEndpoint + "&pwd=" + password
+
+    // Fetch data from the backend API
+    Promise.all([
+        fetchData(monthlyExpensesEndpoint),
+        fetchData(categoryExpensesEndpoint),
+        fetchData(creditCardExpensesEndpoint)
+    ]).then(([monthlyExpenses, categoryExpenses, creditCardExpenses]) => {
+        // Bar Chart
+        document.getElementById("passwordPrompt").style.display = "none";
+        document.getElementById("mainContainer").style.display = "block";
+
+        const barCtx = document.getElementById('barChart').getContext('2d');
+        // monthlyExpenses.sort()
+        const monthLabels = monthlyExpenses ? Object.keys(monthlyExpenses) : []
+        monthLabels.sort((a, b) => {
+            return monthOrder[a] - monthOrder[b];
+        })
+        console.log(monthlyExpenses)
+
+        var expenses = []
+        for (let i = 0; i < monthLabels.length; i++) {
+            console.log(expenses)
+
+            expenses.push(monthlyExpenses[monthLabels[i]])
+        }
+        console.log(monthLabels)
+        console.log(expenses)
+        const barChart = new Chart(barCtx, {
+            type: 'bar',
+            data: {
+                labels: monthLabels,
+                datasets: [{
+                    label: 'Monthly Expenses',
+                    data: expenses,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
+        });
 
-    // Pie Chart for Category Expenses
-    const categoryCtx = document.getElementById('categoryChart').getContext('2d');
-    const categoryChart = new Chart(categoryCtx, {
-        type: 'pie',
-        data: {
-            labels: categoryExpenses ? Object.keys(categoryExpenses) : [],
-            datasets: [{
-                data: categoryExpenses ? Object.values(categoryExpenses) : [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
-                ],
-                borderWidth: 1
-            }]
-        }
-    });
+        // Pie Chart for Category Expenses
+        const categoryCtx = document.getElementById('categoryChart').getContext('2d');
+        const categoryChart = new Chart(categoryCtx, {
+            type: 'pie',
+            data: {
+                labels: categoryExpenses ? Object.keys(categoryExpenses) : [],
+                datasets: [{
+                    data: categoryExpenses ? Object.values(categoryExpenses) : [],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        });
 
-    // Pie Chart for Credit Card Expenses
-    const creditCardCtx = document.getElementById('creditCardChart').getContext('2d');
-    const creditCardChart = new Chart(creditCardCtx, {
-        type: 'pie',
-        data: {
-            labels: creditCardExpenses ? Object.keys(creditCardExpenses) : [],
-            datasets: [{
-                data: creditCardExpenses ? Object.values(creditCardExpenses) : [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                ],
-                borderWidth: 1
-            }]
-        }
+        // Pie Chart for Credit Card Expenses
+        const creditCardCtx = document.getElementById('creditCardChart').getContext('2d');
+        const creditCardChart = new Chart(creditCardCtx, {
+            type: 'pie',
+            data: {
+                labels: creditCardExpenses ? Object.keys(creditCardExpenses) : [],
+                datasets: [{
+                    data: creditCardExpenses ? Object.values(creditCardExpenses) : [],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        });
+    }).catch(function(err) {
+        alert(err.message); // some coding error in handling happened
     });
-});
+}
